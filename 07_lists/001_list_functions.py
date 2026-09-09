011bd97e35b9b5414ec35a7f7601e2d799a0dfa0
ereddy7 <java.eswar01@gmail.com>
2026-09-09 12:42:58 +0530

    output

--- 07_lists/001_list_functions.py
+++ 07_lists/001_list_functions.py
@@ -1,3 +1,30 @@
 x=[10,20,20,30]
+print(x)
 print(len(x),x.count(20),x.index(20))
-x.append(40); x.insert(1,15); x.extend([50,60]); x.remove(20); print(x.pop()); x.reverse(); x.sort(); print(x)
+
+x.append(40); 
+print(x)
+x.insert(1,15); 
+print(x)
+x.extend([50,60]); 
+print(x)
+x.remove(20); 
+print(x)
+print(x.pop()); 
+print(x)
+x.reverse(); 
+print(x)
+x.sort(); 
+print(x)
+
+
+#[10, 20, 20, 30]
+#4 2 1
+#[10, 20, 20, 30, 40]
+#[10, 15, 20, 20, 30, 40]
+#[10, 15, 20, 20, 30, 40, 50, 60]
+#[10, 15, 20, 30, 40, 50, 60]
+#60
+#[10, 15, 20, 30, 40, 50]
+#[50, 40, 30, 20, 15, 10]
+#[10, 15, 20, 30, 40, 50]
--- 07_lists/003_nested_matrix.py
+++ 07_lists/003_nested_matrix.py
@@ -1,4 +1,23 @@
 m=[[10,20,30],[40,50,60],[70,80,90]]
 for row in m:
-    for v in row: print(v,end=" ")
+    for v in row: 
+        print(v)
     print()
+
+m=[[10,20,30],[40,50,60],[70,80,90]]
+for row in m:
+    for v in row: 
+        print(v,end=" ")
+    print()
+
+#40
+#50
+#60
+
+#70
+#80
+#90
+
+#10 20 30 
+#40 50 60 
+#70 80 90 
